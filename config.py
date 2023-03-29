6d320fc2c0fe3d0df14655cb1d8f3d219bef5ea6
xkli <lxk_yb@163.com>
2023-03-29 02:25:39 +0000

    example

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--seed', help='seed everything', type=int, default=2023)
 parser.add_argument('--root', help='data root', type=str, default="./data")
-parser.add_argument('--name', help='data name', type=str, default="Cora")
+parser.add_argument('--name', help='data name', type=str, default="PubMed")                     # CiteSeer, PubMed
 parser.add_argument('--use_cuda', help='use gpu', type=bool, default=True)
 parser.add_argument('--gpu_id', help='gpu id', type=int, default=0)
 parser.add_argument('--num_normalize_train', help='number of normalized train times', type=int, default=1)
@@ -23,9 +23,9 @@ parser.add_argument('--gnn_num_layers', help='number of gnn layers', type=int, d
 parser.add_argument('--gnn_dropout', help='drop out of gnn model', type=float, default=0.5)
 parser.add_argument('--lp_prop', help='prop steps of label propagation', type=int, default=5)
 parser.add_argument('--temperature', help='temperature of label propagation', type=int, default=20)
-parser.add_argument('--moment_type', help='type of moments', type=str, default="hybrid")
-parser.add_argument('--num_moments', help='order of moments', type=int, default=6)
-parser.add_argument('--gm_alpha', help='gm alpha', type=float, default=0.50)
+parser.add_argument('--moment_type', help='type of moments', type=str, default="origin")        # citeseer: hybrid  pubmed: origin
+parser.add_argument('--num_moments', help='order of moments', type=int, default=13)             # citeseer: 20      pubmed: 13
+parser.add_argument('--gm_alpha', help='gm alpha', type=float, default=0.65)                    # citeseer: 0.60    pubmed: 0.65
 
 args = parser.parse_args()
 
